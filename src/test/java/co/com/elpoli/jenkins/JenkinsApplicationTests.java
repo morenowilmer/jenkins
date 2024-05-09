28f988f15e1ffa030d8d0a766877b4148b146fe7
Wilmer Moreno <morenowilmer115@gmail.com>
2024-05-08 23:41:16 -0500

    Inicio

---
+++ src/test/java/co/com/elpoli/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.elpoli.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
